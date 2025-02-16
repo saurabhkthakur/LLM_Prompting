d01fe6324cb1963f2cd010394545a7b89e560c64
saurabhkthakur <thakurksaurabh96@gmail.com>
2025-02-16 14:23:28 +0530

    rag pipeline using chrme db and google api

---
+++ embedding.py
@@ -0,0 +1,25 @@
+import google.generativeai as genai
+
+from chromadb import Documents, EmbeddingFunction, Embeddings
+from google.api_core import retry
+
+
+class GeminiEmbeddingFunction(EmbeddingFunction):
+    document_mode = True
+
+    def __call__(self, input: Documents) -> Embeddings:
+        if self.document_mode:
+            embedding_task = "retrieval_document"
+        else:
+            embedding_task = "retrieval_query"
+
+        retry_policy = {"retry": retry.Retry(predicate=retry.if_transient_error)}
+
+        response = genai.embed_content(
+            model="models/text-embedding-004",
+            content=input,
+            task_type=embedding_task,
+            request_options=retry_policy,
+        )
+        return response["embedding"]
+
